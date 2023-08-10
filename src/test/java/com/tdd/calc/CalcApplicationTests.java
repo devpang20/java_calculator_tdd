7368fe266c6cee193b06cbed028941786b9737cd
sik2 <devpang20@gmail.com>
2023-08-10 09:44:34 +0900

    feat: plus

--- src/test/java/com/tdd/calc/CalcApplicationTests.java
+++ src/test/java/com/tdd/calc/CalcApplicationTests.java
@@ -16,4 +16,17 @@ class CalcApplicationTests {
 		assertThat(CalcApp.run("1 + 1")).isEqualTo(2);
 	}
 
+	@Test
+	@DisplayName("1 + 1 == 6")
+	void t2() {
+		assertThat(CalcApp.run("1 + 1")).isEqualTo(6);
+	}
+
+	@Test
+//	@DisplayName("2 + 2 == 4")
+
+
+	@Test
+//	@DisplayName("1000 + 280 == 1280")
+
 }
