7966eafe7be28e48d85ddd3d11ad683a57ebb41b
sik2 <devpang20@gmail.com>
2023-08-10 10:07:59 +0900

    더feat: plus case test

--- src/test/java/com/tdd/calc/CalcApplicationTests.java
+++ src/test/java/com/tdd/calc/CalcApplicationTests.java
@@ -17,16 +17,20 @@ class CalcApplicationTests {
 	}
 
 	@Test
-	@DisplayName("1 + 1 == 6")
-	void t2() {
-		assertThat(CalcApp.run("1 + 1")).isEqualTo(6);
+	@DisplayName("2 + 1 == 3")
+	void t2 () {
+		assertThat(CalcApp.run("2 + 1")).isEqualTo(3);
 	}
 
 	@Test
-//	@DisplayName("2 + 2 == 4")
-
+	@DisplayName("2 + 2 == 4")
+	void t3 () {
+		assertThat(CalcApp.run("2 + 2")).isEqualTo(4);
+	}
 
 	@Test
-//	@DisplayName("1000 + 280 == 1280")
-
+	@DisplayName("1000 + 280 == 1280")
+	void t4 () {
+		assertThat(CalcApp.run("1000 + 280 == 1280")).isEqualTo(1280);
+	}
 }
