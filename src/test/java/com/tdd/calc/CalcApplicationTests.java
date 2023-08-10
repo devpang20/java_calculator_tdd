8ae6062f18bce1517f72486c43ae88b1bfaeae4e
sik2 <devpang20@gmail.com>
2023-08-10 09:36:55 +0900

    setting calc

--- src/test/java/com/tdd/calc/CalcApplicationTests.java
+++ src/test/java/com/tdd/calc/CalcApplicationTests.java
@@ -1,13 +1,19 @@
 package com.tdd.calc;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 @SpringBootTest
 class CalcApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("1 + 1 == 2")
+	void t1() {
+		assertThat(CalcApp.run("1 + 1")).isEqualTo(2);
 	}
 
 }
